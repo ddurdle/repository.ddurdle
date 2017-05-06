634e85f145ab869a56b6564e96256b3e952efedb
Dylan Durdle <ddurdle@gmail.com>
2017-05-06 08:55:20 -0400

    update

--- sync-gdrive.sh
+++ sync-gdrive.sh
@@ -1,2 +1,2 @@
-rsync -avix --progress --exclude-from '.git' ../XBMC-gdrive/* repo/plugin.video.gdrive
-rsync -avix --progress --exclude-from '.git' ../XBMC-gdrive-0.3-dev/* repo.princeoliver/plugin.video.gdrive
+rsync -avix --progress --exclude-from '.git' ../stable/GDrive-for-KODI/* repo/plugin.video.gdrive
+#rsync -avix --progress --exclude-from '.git' ../XBMC-gdrive-0.3-dev/* repo.princeoliver/plugin.video.gdrive
